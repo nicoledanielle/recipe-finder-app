3c48e038bd55a8ef83192bedf0540c24ecb1c70f
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-14 14:05:13 -0800

    have data returning from api

--- src/actions/recipes.js
+++ src/actions/recipes.js
@@ -1,11 +1,11 @@
-import API_KEY from '../config';
+// import API_KEY from '../config';
 
 export const fetchRecipes = (ingredient) => {
   return() => {
     console.log('fetch recipe running');
-    // fetch(`http://food2fork.com/api/search?key=${API_KEY}&q=${ingredient}`)
-    //   .then(response => response.json())
-    //   .then(data => console.log(data))
-    //   .catch(err => console.log(err));
+    fetch(`http://food2fork.com/api/search?key=47f4cb3436ae592fb10860d0f9ab7a81&q=${ingredient}`)
+      .then(response => response.json())
+      .then(data => console.log(data.recipes))
+      .catch(err => console.log(err));
   }
 }
--- src/components/recipelist.js
+++ src/components/recipelist.js
@@ -5,8 +5,7 @@ import { fetchRecipes } from '../actions/recipes';
 
 export class RecipeList extends React.Component {
   componentWillMount(){
-    console.log('here');
-    // this.props.dispatch(fetchRecipes('broccoli'));
+    this.props.dispatch(fetchRecipes('broccoli'));
   }
 
   render() {
