c0cf884a6709695f45b8b2165743a5f316082848
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-02-16 09:25:34 -0800

    styled home button

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Header(props) {
   return(
     <View style={styles.headContainer}>
+      <View style={styles.iconContainer}>
+        <Icon 
+          name='home'
+          size={30}
+          color='#f1f2f6'
+        />
+      </View>
       <Text style={styles.headText}>{props.headerText}</Text>
     </View>
   )
 };
 
 const styles = StyleSheet.create({
+  iconContainer: {
+    position: 'absolute',
+    left: 20,
+    width: 100,
+    top: 45
+  },
   headContainer: {
     backgroundColor: '#5352ed',
     alignItems: 'center',
